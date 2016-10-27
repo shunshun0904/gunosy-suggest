093751199959d0a5592771bd2157ff6031c2d033
田中　翔 <booo005130@gmail.com>
2016-10-27 13:52:05 +0900

    カプセル化

---
+++ gethtmltext.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError,URLError
+from bs4 import BeautifulSoup
+
+
+def url_to_text(url):
+	#try文とif文でカプセル化します。
+
+	#空のurlが入力されたときの処理
+	if url == '':
+		return None
+	try:
+		html= urlopen(url)
+	#ページがサーバー上で見つからないとき(または取り出すとき)のエラー
+	except HTTPError as e:
+		return None
+	#サーバーが見つからないときのエラー
+	except URLError as e:
+		return None
+	try:
+		bsObj = BeautifulSoup(html.read())
+	#Noneオブジェクトにアクセスしようとするときのエラー
+	except AttributeError as e:
+		return None
+	
+	text = bsObj.find("h1").get_text()
+	return text
+
+
+
--- guesscategory/views.py
+++ guesscategory/views.py
@@ -1,30 +1,31 @@
+#命名規則： PEP8
 from django.http.response import HttpResponse
 from django.shortcuts import render # 追加する
 from datetime import datetime #追加する
 import naivebayes
-import inputscraping
-import scraiping
+import gethtmltext
+import gettrain
 
 
 #Gunosyのサイトをスクレイピングをします。
 nb = naivebayes.NaiveBayes()
 #Gunosyのサイトの記事データを用いて訓練させます。
-scraiping.gunosy_train(nb)
-
+gettrain.gunosy_train(nb)
+# nb.train("ラーメン","食べ物")
 
 
 def hello_guess_category(request):
 
+	#フォームからurlを取得
 	url = request.GET.get('url')
-
-	#urlが入力されると
-	if url != None and url !='':
-		urltext = inputscraping.url_to_text(url)
-		category = "推定カテゴリー ：" + nb.classifier(urltext)
-	#urlが入力していないときの処理。
-	else:
+	#urlのhtmlファイルのテキストを取得
+	html_text = gethtmltext.url_to_text(url)
+	#エラーが出た場合の処理
+	if html_text == None:
 		category = "urlを入力して下さい。"
-
+	#エラが無かった場合の処理
+	else:
+		category = "推定カテゴリー ：" + nb.classifier(html_text)
 
 	d = {
 	    'category': category
