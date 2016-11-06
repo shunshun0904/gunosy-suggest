f14c8799dab78390664087fda9740fbf392834e6
田中　翔 <booo005130@gmail.com>
2016-11-06 16:29:52 +0900

    views.pyがautopep8でpep8に自動修正できなかったところがありましたので（if文の条件式を==ではなくisにしました。）その部分を手動で修正いたしました。　#4  Please enter the commit message for your changes. Lines starting

--- guesscategory/views.py
+++ guesscategory/views.py
@@ -1,4 +1,4 @@
-#命名規則： PEP8
+# 命名規則： PEP8
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
@@ -7,32 +7,26 @@ import gethtmltext
 import gettrain
 
 
-#ナイーブベイズ分類器のオブジェクトを作成。
+# ナイーブベイズ分類器のオブジェクトを作成。
 nb = naivebayes.NaiveBayes()
-#Gunosyのサイトをスクレイピングし、その記事データを用いて訓練させます。
+# Gunosyのサイトをスクレイピングし、その記事データを用いて訓練させます。
 gettrain.gunosy_train(nb)
 
 
 def hello_guess_category(request):
-	#view関数が呼ばれたびにスクレイピングして学習しないようにオブジェクトは外部で作成します。
-	global nb
-	#フォームからurlを取得
-	url = request.GET.get('url')
-	#urlのhtmlファイルのテキストを取得
-	html_text = gethtmltext.url_to_text(url)
-	#エラーが出た場合の処理
-	if html_text == None:
-		category = "urlを入力して下さい。"
-	#エラが無かった場合の処理
-	else:
-		category = "推定カテゴリー ：" + nb.classifier(html_text)
-	d = {
-	    'category': category
-	}
-	return render(request, 'index.html', d)
-
-
-
-
-
-
+    # view関数が呼ばれたびにスクレイピングして学習しないようにオブジェクトは外部で作成します。
+    global nb
+    # フォームからurlを取得
+    url = request.GET.get('url')
+    # urlのhtmlファイルのテキストを取得
+    html_text = gethtmltext.url_to_text(url)
+    # エラーが出た場合の処理
+    if html_text is None:
+        category = "urlを入力して下さい。"
+    # エラが無かった場合の処理
+    else:
+        category = "推定カテゴリー ：" + nb.classifier(html_text)
+    d = {
+        'category': category
+    }
+    return render(request, 'index.html', d)
