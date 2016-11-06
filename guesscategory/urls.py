7a613d318e065799115f5dff628d1c2759a2334e
田中　翔 <booo005130@gmail.com>
2016-11-06 16:37:15 +0900

    urls.pyのautopep8による自動修正化と手動による修正を行いました。 #4

--- guesscategory/urls.py
+++ guesscategory/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello_guess_category, name = 'hello_guess_category'),
+    url(r'^$', views.hello_guess_category, name='hello_guess_category'),
 ]
