366f8d46e5d0cb978a5d706c3da9effd3971c3d1
shunshun0904 <human.h.uw0904@gmail.com>
2017-08-04 04:40:18 +0000

    形態素解析ファイル

--- morphological.py
+++ morphological.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # 命名規則： PEP8
+# YahooのAPIを利用して形態素解析
 import requests
 from xml.etree.ElementTree import *
 import csv
@@ -8,13 +9,13 @@ import csv
 f = open('vocaburary.csv', 'w')
 csvWriter = csv.writer(f)
 
+
 def split(body):
-    # YahooのAPIを利用して形態素解析
     request_URL = "http://jlp.yahooapis.jp/MAService/V1/parse"
-    parameter = {'appid': """dj00aiZpPUxFRFVpcmx2UnpLNiZzPWNvbnN1bWVyc2VjcmV0Jng9ODc-""",
+    parameter = {'appid': """dj00aiZpPUxFRFVpcmx2UnpLNiZ
+    zPWNvbnN1bWVyc2VjcmV0Jng9ODc-""",
                  'sentence': body,
                  'results': 'ma',
-                # 'filter': '1|2|3|4|5|9|10'}
                  'filter': '1|2|9|10'}
     r = requests.get(request_URL, params=parameter)
 
