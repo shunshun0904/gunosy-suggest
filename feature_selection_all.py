679225522af2a1d47112b53c7ffdb39b31b7b50d
shunshun0904 <human.h.uw0904@gmail.com>
2017-08-04 06:35:46 +0000

    精度比較のグラフ

--- feature_selection_all.py
+++ feature_selection_all.py
@@ -2,26 +2,16 @@ import codecs
 import sys
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
-import crossvalidation_stopword
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-print("accuracy:", average)
+
+
 height = np.array([0.7791, 0.7690, 0.6713, 0.6256])
 left = np.array([1, 2, 3, 4])
 label = ["Only_stopwords", "Nothing", "Chi-squared", "Mutual-info"]
 plt.bar(left, height, tick_label=label, align="center")
 for x, y in zip(left, height):
-    plt.text(x, y, y, ha='center', va='bottom')
-
-height = np.array([0.7791878172588832,
-                   0.7690355329949239,
-                   0.6713197969543148,
-                   0.6256345177664975])
-left = np.array([1, 2, 3, 4])
-label = ["Only_stopwords", "Nothing", "Chi-squared", "Mutual-info"]
-plt.bar(left, height, tick_label=label, align="center")
-for x, y in zip(X, elapsed):
     plt.text(x, y, y, ha='center', va='bottom')
 
 plt.ylabel("accuracy")
