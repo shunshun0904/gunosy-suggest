ed308b4af3c0e8cebb70b7da2aba650616adaf95
shunshun0904 <human.h.uw0904@gmail.com>
2017-08-04 04:55:34 +0000

    精度比較のグラフ

--- feature_selection_all.py
+++ feature_selection_all.py
@@ -4,19 +4,20 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import crossvalidation_stopword
 
-
-height = np.array([ 0.7791878172588832,0.7690355329949239,0.6713197969543148,0.6256345177664975])
+print ("accuracy:", average)
+height = np.array([ 0.7791,0.7690,0.6713,0.6256])
 left = np.array([1,2,3,4])
 label = ["Only_stopwords","Nothing","Chi-squared", "Mutual-info"]
 plt.bar(left, height,tick_label=label, align="center")
-for x, y in zip(X, elapsed):
+for x, y in zip(left, height):
     plt.text(x, y, y, ha='center', va='bottom')
-    
+
 plt.ylabel("accuracy")
 plt.title("Comparison of Stopwords")
 plt.legend()
 plt.ylim(ymax = 1.0, ymin = 0)
 plt.show()
-filename = "comparison_stopwords.png"
+filename = "comparison_stopwords-hoge.png"
 plt.savefig(filename)
