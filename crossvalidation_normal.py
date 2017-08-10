467c783a0df617fafb005fd62516552b6830b873
shunshun0904 <human.h.uw0904@gmail.com>
2017-08-10 02:29:32 +0000

    交差検定

--- crossvalidation_normal.py
+++ crossvalidation_normal.py
@@ -1,12 +1,14 @@
 # coding:utf-8
+# 特徴選択なし、ストップワード除去なしの交差検定のプログラム
 import codecs
 import sys
+import math
 import matplotlib
 import matplotlib.pyplot as plt
 from naivebayes_normal import NaiveBayes  # 特徴選択なしのナイーブベイズを使う
 matplotlib.use('Agg')
 
-# 特徴語抽出数
+# 特徴語抽出数（このプログラムでは関係ないのでいくつでも良い）
 K = 500
 
 # データの行数を取得
@@ -19,8 +21,6 @@ fp.close()
 num = len(number)
 
 # 交差検定のプログラム
-
-
 def crossValidation(data, N=num, randomize=False):
     """N-fold Cross Validationで分類精度を評価"""
     # データをシャッフル
@@ -37,7 +37,6 @@ def crossValidation(data, N=num, randomize=False):
         # ナイーブベイズ分類器を学習
         nb = NaiveBayes(K)
         nb.train(trainData)
-
         # テストデータの分類精度を計算
         hit = 0
         numTest = 0
@@ -52,6 +51,7 @@ def crossValidation(data, N=num, randomize=False):
         accuracyList.append(accuracy)
     # N回の平均精度を求める
     average = sum(accuracyList) / float(N)
+    average_f = round(average,4)
     return average
 
 
@@ -66,4 +66,5 @@ if __name__ == "__main__":
     fp.close()
     # N-fold Cross Validationで分類精度を評価
     average = crossValidation(data, N=num, randomize=True)
-    print("accuracy:", average)
+    average_f = round(average,4)
+    print(average_f)
